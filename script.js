7c01eb2b9dcc20dfcb686089796a242d8cf6ec69
Quadas <musadaq23@hotmail.com>
2023-01-05 18:46:19 -0500

    edited Js file

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+//show current date for each day
+$("#currentDay").html(moment().format('dddd, MMMM Do'));
+
+//for loop to determine color for each hour
+for (var i = 9; i < 18; i++) {
+    if (i < moment().hour()) {
+        $("#" + i).addClass("past")
+    }
+    else if (i === moment().hour()) {
+        $("#" + i).addClass("present")
+    }
+    else {
+        $("#" + i).addClass("future")
+    }
+}
+
+//click eventlistener to save info to local storage
+$("button").on("click", function (event) {
+    event.preventDefault();
+    var textEvent = $("textarea#" + this.id).val();
+    localStorage.setItem("calendarItem" + this.id, textEvent)
+});
+
+//add text values from localstorage into appropriate hour
+$(document).ready(function () {
+    for (i = 9; i < 18; i++) {
+        var savedText = localStorage.getItem("calendarItem" + i);
+        $("textarea#" + i).html(savedText);
+    }
 });
